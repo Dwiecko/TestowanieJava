0bc1642c227384d792866bcc2c62d504126ec795
Dawid Więcko <dwiecko@sigma.ug.edu.pl>
2018-05-09 20:59:01 +0200

    Update MessageServiceImplementation.java

--- src/main/java/com/example/mockdemo/messenger/MessageServiceImplementation.java
+++ src/main/java/com/example/mockdemo/messenger/MessageServiceImplementation.java
@@ -10,19 +10,10 @@ public class MessageServiceImplementation implements MessageService {
 	}
 
 	public SendingStatus send(String server, String message) throws MalformedRecipientException {
-        if(isNullable(server) || server.trim().length() < 4) 
-            throw new MalformedRecipientException();//server must have at least 1 letter and ends with .pl
-        
-        if(isNullable(message) || message.trim().length() < 3) 
-            throw new MalformedRecipientException();//messsage must have at least 3 letters
-
-        if (checkConnection(server) == ConnectionStatus.FAILURE) {
-            return SendingStatus.SENDING_ERROR;
-        }
-
-        else if (checkConnection(server) == ConnectionStatus.SUCCESS) {
-			return SendingStatus.SENT;
-		}
+        if(isNullable(server) || server.trim().length() < 4) throw new MalformedRecipientException();//server must have at least 1 letter and ends with .pl  
+        if(isNullable(message) || message.trim().length() < 3) throw new MalformedRecipientException();//messsage must have at least 3 letters
+        if (checkConnection(server) == ConnectionStatus.FAILURE) return SendingStatus.SENDING_ERROR;
+            else if (checkConnection(server) == ConnectionStatus.SUCCESS) return SendingStatus.SENT;
         return SendingStatus.SENDING_ERROR;
     }
     
